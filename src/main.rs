5eb9a248fcc05ea3b6785bf858b58dc4ae771ce9
Medhy Vinceslas <medhyvinceslas@pop-os.localdomain>
2022-06-08 18:34:15 +0200

    Mimick the matlab quadrotor weight to avoid tuning this

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,23 @@ use quadrotor::vehicle::Quadrotor;
 // use nalgebra::Matrix3;
 // use nalgebra::Vector3;
 
+// from sim in matlab
+// gravity: 9.8100
+//           mass: 0.1800
+//     arm_length: 0.0860
+//          u_min: 0
+//          u_max: 2.1190
 
 
-fn altitude_controller(dt: &f32,
-                       state: &HashMap<&str, f32>,
+
+fn altitude_controller(state: &HashMap<&str, f32>,
                        state_des: &HashMap<&str, f32>,
                        params: &mut HashMap<&str, f32>,
                        mass: &f32) -> f32{
     
-    
-
-
     let position_error = state_des["z"] - state["z"];
     let velocity_error = state_des["z_dot"] - state["z_dot"];
-    params.insert("integral_error_z", params["integral_error_z"] + position_error * dt);
+    params.insert("integral_error_z", params["integral_error_z"] + position_error * params["dt"]);
 
     let acc_z = state_des["z_ddot"] 
                 + params["kv_z"] * velocity_error 
@@ -34,59 +37,12 @@ fn altitude_controller(dt: &f32,
     u1
 }
 
-fn main() {
-    let mut points: Vec<(f64, f64)> = Vec::new();
-    let mut targets: Vec<(f64, f64)> = Vec::new();
 
-    let mass: f32 = 0.057;
-
-    let mut params: HashMap<&str, f32> = HashMap::from([
-        ("integral_error_z", 0.0),
-        ("gravity", 9.81),
-        ("kv_z", 15.0),
-        ("kp_z", 50.0),
-        ("ki_z", 8.0),
-    ]);
-    
-    let mut state: HashMap<&str, f32> = HashMap::from([
-        ("z", 0.0),
-        ("z_dot", 0.0),
-        ("z_ddot", 0.0)
-    ]);
-
-    let mut state_des: HashMap<&str, f32> = HashMap::from([
-        ("z", 10.0),
-        ("z_dot", 0.01),
-        ("z_ddot", 0.0)
-    ]);
-
-    let dt: f32 = 0.1;
-    
-
-    let mut quadrotor = Quadrotor::new(mass, state);
-    let m = quadrotor.mass;
-    let mg = m * params["gravity"];
-
-
-    for _x in 0..1000{
-        let u1 = altitude_controller(&dt, &quadrotor.state, &state_des, &mut params, &m);
-        let z_ddot = (u1 - mg) / m;
-        
-        // plot
-        points.push((_x as f64, quadrotor.state["z"] as f64));
-        if _x % 15 == 0 {
-            targets.push((_x as f64, state_des["z"] as f64));
-        }
-        
-        quadrotor.update_state(dt, z_ddot);
-        // advance_state(self, dt):
-        // quadrotor.state.insert("z_ddot", z_ddot);
-        // quadrotor.state.insert("z_dot", quadrotor.state["z_dot"] + quadrotor.state["z_ddot"] * dt);
-        // quadrotor.state.insert("z", quadrotor.state["z"] + quadrotor.state["z_dot"] * dt);
-    }
-    
-
-
+fn plot_trajectories(points: Vec<(f64, f64)>,
+                     targets: Vec<(f64, f64)>,
+                     min_x: f64, max_x: f64,
+                     min_y: f64, max_y: f64,
+                     filename: &str){
     let plot_target: Plot = Plot::new(targets).point_style(
         PointStyle::new()
         .colour("#000000")
@@ -103,11 +59,54 @@ fn main() {
 
     let canvas = ContinuousView::new()
         .add(plot_target).add(plot_state)
-        .x_range(0., 1000.)
-        .y_range(0., state_des["z"] as f64 + 10.0)
+        .x_range(min_x, max_x)
+        .y_range(min_y, max_y)
         .x_label("Iteration")
         .y_label("Altitude Z");
 
-    Page::single(&canvas).save("pid_altitude.svg").unwrap();
+    Page::single(&canvas).save(filename).unwrap();
+}
 
+/* ----------------------------------------------------- */
+
+fn main() {
+    let mut points: Vec<(f64, f64)> = Vec::new();
+    let mut targets: Vec<(f64, f64)> = Vec::new();
+    
+    let mut params: HashMap<&str, f32> = HashMap::from([
+        ("integral_error_z", 0.0),
+        ("gravity", 9.81),
+        ("kv_z", 40.0),
+        ("kp_z", 130.0),
+        ("ki_z", 0.0),
+        ("dt", 0.1)
+    ]);
+    
+    let initial_state: HashMap<&str, f32> = HashMap::from([
+        ("z", 0.0), ("z_dot", 0.0), ("z_ddot", 0.0)
+    ]);
+
+    let state_des: HashMap<&str, f32> = HashMap::from([
+        ("z", 10.0), ("z_dot", 0.01), ("z_ddot", 0.0)
+    ]);
+    
+    
+    let mut quadrotor = Quadrotor::new(0.18, initial_state);
+    let m = quadrotor.mass;
+
+    for _x in 0..1000{
+        let u1 = altitude_controller(&quadrotor.state, &state_des, &mut params, &m);
+        let u1 = u1.clamp(quadrotor.u_min, quadrotor.u_max);
+        
+        points.push((_x as f64, quadrotor.state["z"] as f64));
+        if _x % 15 == 0 {
+            targets.push((_x as f64, state_des["z"] as f64));
+        }
+
+        quadrotor.update_state(&params["dt"], &u1);
+        println!("{}", quadrotor.state["z"]);
+    }
+    
+    let (min_x, max_x, min_y, max_y) = (0.0, 1000.0, 0.0, state_des["z"] as f64 + 10.0);
+    plot_trajectories(points, targets, min_x, max_x, min_y, max_y, "pid_altitude.svg");
 }
--- src/quadrotor.rs
+++ src/quadrotor.rs
@@ -4,15 +4,23 @@ pub mod vehicle {
     pub struct Quadrotor<'a> {
         pub state: HashMap<&'a str, f32>,
         pub mass: f32,
+        pub u_min: f32,
+        pub u_max: f32
     }
 
     impl<'a> Quadrotor<'a> {
         
         pub fn new(mass: f32, initial_state: HashMap<&'a str, f32>) -> Quadrotor<'a> {
-            Quadrotor::<'a> { mass: mass, state: initial_state }
+            Quadrotor::<'a> {
+                mass: mass,
+                state: initial_state,
+                u_min: 0.0,
+                u_max: 1.2 * mass * 9.81
+            }
         }
 
-        pub fn update_state(&mut self, dt: f32, z_ddot: f32) -> () {
+        pub fn update_state(&mut self, dt: &f32, u1: &f32) -> () {
+            let z_ddot = u1 / &self.mass - 9.81;
             let _ = &self.state.insert("z_ddot", z_ddot);
             let _ = &self.state.insert("z_dot", &self.state["z_dot"] + &self.state["z_ddot"] * dt);
             let _ = &self.state.insert("z", &self.state["z"] + &self.state["z_dot"] * dt);
